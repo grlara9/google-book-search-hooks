e44b9e214f539c7caf1f9417d29907fb6f421d32
Guillermo <grlara9@gmail.com>
2021-08-21 20:40:30 -0500

    finish with actions on reducer to add and fetch data to console

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,6 +1,29 @@
+import {useReducer} from 'react'
+
+const ACTIONS = {
+    REQUEST_DATA: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+
+const reducer =(state, action)=>{
+    switch(action.type){
+        case ACTIONS.REQUEST_DATA:
+            return{loading: true, jobs: []}
+        case ACTIONS.GET_DATA:
+            return {...state, loading: false, books: action.payload.books}
+        case ACTIONS.ERROR:
+            return {...state, loading:false, books:[]}
+
+        default:
+            return state
+    }
+}
 export default function reducer(params, page){
+
+    const [state, dispatch] = useReducer(reducer, {books: [], loading: false, error: false})
     return{
-        books:[2,3,4,5,6],
+        books:[],
         loading:true,
         error: true
     }
