766e68acd90e984b07c3e98b2f08e872552cb4c7
Guillermo <grlara9@gmail.com>
2021-08-24 08:56:45 -0500

    map through books arrays and create Books component will display all books available

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import {useState} from 'react'
 import useFetchBooks from './components/Reducer';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-
+  const [params, setParams] =useState({})
+  const [page, setPage] = useState(1)
   const {books, loading, error} = useFetchBooks()
   return (
+
    <Container>
-     {loading && <h1>Loading</h1>}
-     {error &&<h1> Error. Try refreshing</h1>}
-      <h1>{books.length}</h1>
+     {loading && <h1>Loading...</h1>}
+     {error && <h1> Error. Try refreshing</h1>}
+      {books.map(book =>{
+        <Books key={book.id} book={book} />
+      })}
    </Container>
   );
 }
