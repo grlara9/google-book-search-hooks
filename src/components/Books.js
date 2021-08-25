9c054e6c600dfe0fe010deef66eb96fa6ca1f904
Guillermo <grlara9@gmail.com>
2021-08-24 23:39:28 -0500

    pass more props to book component and apply css

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import {Card , Badge } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
 //rafc
  const Books = ({ book }) => {
     
@@ -10,9 +11,16 @@ import { Card } from 'react-bootstrap'
                     <div>
                         <Card.Title>
                             {book.volumeInfo.title} - <span className="text-muted
-                            font-weight-light">{book.volumeInfo.authors}</span>
-
+                            font-weight-light" >{book.volumeInfo.authors}</span>
                         </Card.Title>
+                        <Card.Subtitle className="text-muted mb-1">
+                            {new Date(book.volumeInfo.publishedDate).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="info">{book.volumeInfo.categories}</Badge>
+
+                        <div>
+                           <ReactMarkdown source={book.volumeInfo.previewLink} />
+                        </div>
                     </div>
                 </div>
 
