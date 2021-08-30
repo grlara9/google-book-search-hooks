8668d1352d8ea39acaebb353ad1c243dfc9193ea
Guillermo <grlara9@gmail.com>
2021-08-29 21:27:29 -0500

    added more details to my card js

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,36 +2,32 @@ import  React,{ useState} from 'react'
 import {Card , Badge, Button, Collapse } from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
 //rafc
-
+import './Books.css'
  const Books = ({ book }) => {
 
     const [open, setOpen]= useState(false)
-
-    const toggle = () => setOpen(!open)
+    
     return (
         <Card className="mb-3">
-      <Card.Body>
-        <div className="d-flex justify-content-between">
-          <div>
-            <Card.Title>
-              {book.volumeInfo.title} - <span className="text-muted font-weight-light">{book.volumeInfo.authors}</span>
-            </Card.Title>
-            <Card.Subtitle className="text-muted mb-2">
-              {new Date(book.volumeInfo.publishedDate).toLocaleDateString()}
-            </Card.Subtitle>
-            <Badge variant="secondary" className="mr-2">ghj</Badge>
-            <div style={{ wordBreak: 'break-all' }}>
-              <ReactMarkdown  />
+          <Card.Body>
+           <div className="d-flex justify-content-between">
+              <div>
+                  <Card.Title>
+                    {book.volumeInfo.title} - <span className="text-muted font-weight-light">{book.volumeInfo.authors}</span>
+                  </Card.Title>
+                  <Card.Subtitle className="text-muted mb-2">
+                    {new Date(book.volumeInfo.publishedDate).toLocaleDateString()}
+                  </Card.Subtitle>
+                  <Badge><span color="black">{book.volumeInfo.categories}</span></Badge>
+                  <div>
+                    <ReactMarkdown>{book.volumeInfo.previewLink}</ReactMarkdown>
+                  </div>
+              </div>
+              <img className="d-none d-md-block" height="150"src={book.volumeInfo.imageLinks.thumbnail}/>
             </div>
-          </div>
-          <img className="d-none d-md-block" height="50" />
-        </div>
         <Card.Text>
-          <Button
-            onClick={() => setOpen(prevOpen => !prevOpen)}
-            variant="primary"
-          >
-            {open ? 'Hide Details' : 'View Details'}
+          <Button onClick={() => setOpen(prevOpen => !prevOpen)} variant="primary">
+              {open ? 'Hide Details' : 'View Details'}
           </Button>
         </Card.Text>
         <Collapse in={open}>
